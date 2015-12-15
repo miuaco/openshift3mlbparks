35593a49900b28c260dee61a989e11ef069d0e64
Shuichi Miura <miu.aco.software@gmail.com>
2015-12-15 15:36:58 +0900

    Update JaxrsConfig.java

--- src/main/java/org/openshift/mlbparks/rest/JaxrsConfig.java
+++ src/main/java/org/openshift/mlbparks/rest/JaxrsConfig.java
@@ -3,7 +3,7 @@ package org.openshift.mlbparks.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-// Added by MiuAco
+// Added by MiuAco for OpenShift webhook deploy.
 @ApplicationPath("/ws")
 public class JaxrsConfig extends Application{
 
